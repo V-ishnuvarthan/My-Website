23cbebbe9f6e23398f5cf63f7ddc2f55e145d49a
Vishnu Varthan <vishnuvarthan88707@gmail.com>
2024-02-10 12:44:56 +0530

    mirror2

--- blocks/mirror/mirror.js
+++ blocks/mirror/mirror.js
@@ -3,17 +3,17 @@ export default function decorate(block) {
     console.log(i);
     const col1 = row.children[0];
     console.log(col1);
-    const col1Div = document.createElement("div");
-    col1.className(`row-${i}-col-1`);
+    const col1Div = document.createElement('div');
+    col1.classList.add(`row-${i}-col-1`);
     col1Div.append(...col1.children);
 
     const col2 = row.children[1];
-    const col2Div = document.createElement("div");
-    col2.className(`row-${i}-col-2`);
+    const col2Div = document.createElement('div');
+    col2.classList.add(`row-${i}-col-2`);
     col2Div.append(...col2.children);
 
-    const stepWrapper = document.createElement("div");
-    stepWrapper.className(`step-${i} step-container`);
+    const stepWrapper = document.createElement('div');
+    stepWrapper.classList.add(`step-${i} step-container`);
     stepWrapper.append(col1, col2);
   });
 }
