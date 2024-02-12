b80ddea9086242edbeccf952475e25a2e0a8eb04
Vishnu Varthan <vishnuvarthan88707@gmail.com>
2024-02-12 20:00:36 +0530

    show more

--- blocks/gallery/gallery.js
+++ blocks/gallery/gallery.js
@@ -1,8 +1,22 @@
 export default function decorate(block) {
   const button = document.createElement('a');
-  button.classList.add('button');
+  button.classList.add('button', 'show-more');
   button.setAttribute('href', '#');
   button.textContent = 'Show More..';
   const gallery = document.querySelector('.gallery');
   gallery.append(button);
+
+  const showMore = document.querySelector('.show-more');
+  const images = document.querySelectorAll('.gallery > div');
+  console.log(images.length);
+  for (let i = 8; i < images.length; i++) {
+    images[i].classList.add('hide');
+  }
+
+  showMore.addEventListener('click', function () {
+    for (let i = 8; i < images.length; i++) {
+      images[i].classList.remove('hide');
+    }
+    showMore.style.display = 'none';
+  });
 }
