13a52e64292914a8e572edf1591783f1a769c532
Vishnu Varthan <vishnuvarthan88707@gmail.com>
2024-02-12 19:45:23 +0530

    js

--- blocks/gallery/gallery.js
+++ blocks/gallery/gallery.js
@@ -1,6 +1,7 @@
 export default function decorate(block) {
   const button = document.createElement('a');
   button.classList.add('button');
+  button.setAttribute('href', '#');
   button.textContent = 'Show More..';
   const gallery = document.querySelector('.gallery');
   gallery.append(button);
