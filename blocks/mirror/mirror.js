ef9c95cd2edc0823faaba6e32ced9a6545557c03
Vishnu Varthan <vishnuvarthan88707@gmail.com>
2024-02-12 17:47:18 +0530

    ch

--- blocks/mirror/mirror.js
+++ blocks/mirror/mirror.js
@@ -12,18 +12,3 @@ export default function decorate(block) {
     });
   });
 }
-
-// Select the <h2> element
-const h2Element = document.querySelector('h2');
-
-// Select the <div> element with class "mirror"
-const mirrorDiv = document.querySelector('.mirror');
-
-// Select the parent element of the <h2> element
-//const parentOfH2 = querySelector('.mirror >');
-
-// Append the <h2> element as a child of the <div> element
-mirrorDiv.appendChild(h2Element);
-
-// Remove the <h2> element from its original parent
-//parentOfH2.remove('');
